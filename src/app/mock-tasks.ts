17e5061b515a991b78312e3b3ffd674c0a50e6fe
lavenwahhab <104763311+lavenwahhab@users.noreply.github.com>
2023-03-21 13:55:25 +0100

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+      id: 1,
+      text: "Doctor appointment",
+      day: "May 5th at 3:00 pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "May 12th at 6:00 pm",
+      reminder: false
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      day: "May 19th at 1:00 pm",
+      reminder: true
+    }
+  ];
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import {Observable} from 'rxjs'
+import { Task } from '../Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Contenet-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks'
+
+  constructor(private http:HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+   return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(el: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${el.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+ addTask(el: Task): Observable<Task> {
+  return this.http.post<Task>(this.apiUrl, el,httpOptions);
+ }
+}
